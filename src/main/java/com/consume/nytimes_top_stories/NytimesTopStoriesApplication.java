65215f0305299f1b5bd6bee9eb569d0ea2fdcb3e
Mysnikol <mysnikol@gmail.com>
2020-05-21 10:30:08 -0500

    Initial Commit

---
+++ src/main/java/com/consume/nytimes_top_stories/NytimesTopStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.consume.nytimes_top_stories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NytimesTopStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NytimesTopStoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consume/nytimes_top_stories/NytimesTopStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consume.nytimes_top_stories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NytimesTopStoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
